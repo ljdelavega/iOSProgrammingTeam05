a83a9e0ad3fc53eb7bd9e04838d631680f5cb5e1
Lester Dela Vega <lester.delavega@gmail.com>
2015-10-19 22:42:15 -0600

    Programmer message: Initial creation of Xcode project for iOS App project. Just a simple iOS application in swift that has a tab view.

---
+++ CPSC 599 App Project/CPSC 599 App Project/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  CPSC 599 App Project
+//
+//  Created by Lester Dela Vega on 2015-10-19.
+//  Copyright © 2015 Team 05. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CPSC 599 App Project/CPSC 599 App Project/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CPSC 599 App Project
+//
+//  Created by Lester Dela Vega on 2015-10-19.
+//  Copyright © 2015 Team 05. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
