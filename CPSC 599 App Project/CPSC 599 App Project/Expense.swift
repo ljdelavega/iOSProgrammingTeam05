b02cde23c08fcc18470fefff28696b7e9693e36f
Lester Dela Vega <lester.delavega@gmail.com>
2015-10-26 13:48:13 -0600

    Programmer message: Adding template data model files for Budget, Goal, Expense, and Income. TODO: Edit these classes to have the correct fields. Testing Notes: Try running the program and test persistence of data.

---
+++ CPSC 599 App Project/CPSC 599 App Project/Expense.swift
@@ -0,0 +1,83 @@
+//
+//  Expense.swift
+//  CPSC 599 App Project
+//
+//  Created by Lester Dela Vega on 2015-10-26.
+//  Copyright © 2015 Team 05. All rights reserved.
+//
+
+import UIKit
+
+class Expense: NSObject, NSCoding {
+    
+    /* Fields:
+    Amount
+    Title
+    Date
+    Description
+    Payee
+    Repeating flag
+    Monthly
+    Weekly
+    Daily
+    */
+    
+    // MARK: Properties
+    // Put fields pertaining to the data model class here.
+    var name: String
+    var photo: UIImage?
+    var rating: Int
+    
+    // MARK: Archiving Paths
+    
+    static let DocumentsDirectory = NSFileManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.URLByAppendingPathComponent("expenses")
+    
+    // MARK: Types
+    
+    struct PropertyKey {
+        // Property keys used to encode and decode data
+        static let nameKey = "name"
+        static let photoKey = "photo"
+        static let ratingKey = "rating"
+    }
+    
+    // MARK: Initialization
+    
+    init?(name: String, photo: UIImage?, rating: Int) {
+        // Initialize stored properties.
+        self.name = name
+        self.photo = photo
+        self.rating = rating
+        
+        super.init()
+        
+        // Initialization should fail if there is no name or if the rating is negative.
+        if name.isEmpty || rating < 0 {
+            return nil
+        }
+    }
+    
+    // MARK: NSCoding
+    
+    func encodeWithCoder(aCoder: NSCoder) {
+        // encode the fields that we want to save and persist
+        aCoder.encodeObject(name, forKey: PropertyKey.nameKey)
+        aCoder.encodeObject(photo, forKey: PropertyKey.photoKey)
+        aCoder.encodeInteger(rating, forKey: PropertyKey.ratingKey)
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        // initialize by decoding an existing object from persistent data.
+        let name = aDecoder.decodeObjectForKey(PropertyKey.nameKey) as! String
+        
+        // Because photo is an optional property of Meal, use conditional cast.
+        let photo = aDecoder.decodeObjectForKey(PropertyKey.photoKey) as? UIImage
+        
+        let rating = aDecoder.decodeIntegerForKey(PropertyKey.ratingKey)
+        
+        // Must call designated initializer.
+        self.init(name: name, photo: photo, rating: rating)
+    }
+
+}
