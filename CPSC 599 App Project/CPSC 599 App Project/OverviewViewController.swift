b4e4b2888b6fed7e4427feaaeda7abe4a0c75652
Lester Dela Vega <lester.delavega@gmail.com>
2015-10-27 13:42:21 -0600

    Programmer message: Adding new view controllers that are attached to the 4 tab buttons in the tab bar. TODO: Implement the Overview, Transactions, Budgets, and Goals screens.
    Testing notes: try running using the simulator on various machines.

--- CPSC 599 App Project/CPSC 599 App Project/OverviewViewController.swift
+++ CPSC 599 App Project/CPSC 599 App Project/OverviewViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class OverviewViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +20,15 @@ class FirstViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    /*
+    // MARK: - Navigation
+    
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
+    }
+    */
 
 }
 
