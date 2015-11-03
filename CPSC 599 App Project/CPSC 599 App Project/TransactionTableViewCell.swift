82a31a409dd88542d1bbd9337409205ccdff926c
BY <byipster@hotmail.com>
2015-11-02 17:19:13 -0700

    Programmer's Message: Added 2 files TransactionTableViewCell and TransactionTableViewController - basic table view
    Files changed: Transaction.storyboard

---
+++ CPSC 599 App Project/CPSC 599 App Project/TransactionTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TransactionTableViewCell.swift
+//  CPSC 599 App Project
+//
+//  Created by Brandon Yip on 2015-11-02.
+//  Copyright © 2015 Team 05. All rights reserved.
+//
+
+import UIKit
+
+class TransactionTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var imageview: UIImageView!
+    @IBOutlet weak var priceLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
