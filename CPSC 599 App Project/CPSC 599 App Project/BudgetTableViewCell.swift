93d089e781a3414521b301fc7cd01230322d7d12
ngmichaeLr <Home-AX50@Home-AX50s-Mac.local>
2015-11-04 13:09:54 -0700

    Added TableViewCell and Constraints.

---
+++ CPSC 599 App Project/CPSC 599 App Project/BudgetTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  BudgetTableViewCell.swift
+//  Budget Balancer
+//
+//  Created by Home-AX50 on 2015-11-04.
+//  Copyright © 2015 Team 05. All rights reserved.
+//
+
+import UIKit
+
+class BudgetTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
